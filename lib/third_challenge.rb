228e02516d64d560585a5264e7b89c19874b6b95
Siobhan Mahoney <siobhan.p.mahoney@gmail.com>
2017-10-30 17:19:37 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -18,7 +18,7 @@ def third_challenge
    },
    :capulet => {
       :patriarch => {
-        name: "Lord Capulet"
+        name: "Lord Capulet",
         age: "50"
         },
       :matriarch => {
