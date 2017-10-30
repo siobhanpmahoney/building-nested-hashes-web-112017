9ba8396c969fffb38d92d8ec943377691406f282
Siobhan Mahoney <siobhan.p.mahoney@gmail.com>
2017-10-30 17:17:42 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -26,8 +26,9 @@ def third_challenge
         age: "51"
         },
       :heroine => {
-        name:
-        age:
+        name: "Juliet",
+        age: "15",
+        status: "alive"
         },
       :heroine_friends => []
    }
